a79d3102b6bc0c66f3a73ec115dade82ff866620
Teb Soto <54040004+Yu-Tony@users.noreply.github.com>
2021-04-12 01:14:44 -0500

    v3
    Cambios a las vistas de las pantallas

--- Sidebar/sidebar.js
+++ Sidebar/sidebar.js
@@ -1,23 +1,8 @@
-jQuery(function ($) {
+$(function () {
+	var links = $('.sidebar-links > div');
 
-    $(".sidebar-dropdown > a").click(function() 
-    {
-    $(".sidebar-submenu").slideUp(200);
-    
-    if ($(this).parent().hasClass("active")) 
-    {
-        $(".sidebar-dropdown").removeClass("active");
-        $(this).parent().removeClass("active");
-    } 
-
-    else 
-    {
-
-        $(".sidebar-dropdown").removeClass("active");
-        $(this).next(".sidebar-submenu").slideDown(200);
-        $(this).parent().addClass("active");
-
-    }
-    });
-
+  links.on('click', function () {
+		links.removeClass('selected');
+		$(this).addClass('selected');
+	});
 });
